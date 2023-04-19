5b12b10fd9cc0dd63d2c5298d5c1368c83224811
Lojowski Boris <lojowski.boris@gmail.com>
2023-04-19 14:46:08 +0200

    add neotest

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,28 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  "nvim-neotest/neotest",
+  config = function()
+    -- get neotest namespace (api call creates or returns namespace)
+    local neotest_ns = vim.api.nvim_create_namespace "neotest"
+    vim.diagnostic.config({
+      virtual_text = {
+        format = function(diagnostic)
+          local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+          return message
+        end,
+      },
+    }, neotest_ns)
+    require("neotest").setup {
+      adapters = {
+        require "neotest-rust",
+        require "neotest-jest",
+      },
+    }
+  end,
+  ft = { "rust", "jest" },
+  dependencies = {
+    "rouge8/neotest-rust",
+    "nvim-neotest/neotest-jest",
+  },
 }
